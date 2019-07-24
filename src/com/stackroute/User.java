7d610592689d54a0111fec72081b40f7ab7c07a6
shiv.prakash <shiv.prakash@cgi.com>
2019-07-24 17:45:04 +0530

    Modified do while loop

--- src/com/stackroute/User.java
+++ src/com/stackroute/User.java
@@ -4,21 +4,27 @@ import java.util.Scanner;
 
 public class User {
 
-    public static void main(String args[]){
-        System.out.println("Enter 1 for ON and 2 For OFF ");
-        Scanner sc =new Scanner(System.in);
-        int input=sc.nextInt();
-       Bulb obj=new Bulb();
-        switch (input) {
-            case 1:obj.bulbOn();
-                break;
-            case 2:
-                obj.bulbOff();
-                break;
-            default:
-                System.out.println("enter valid input or exit");
-        }
+    public static void main(String args[]) {
+        int input;
+        do {
+            System.out.println("Enter 1 for ON, 2 For OFF and 0 to exit");
+            Scanner sc = new Scanner(System.in);
+            input = sc.nextInt();
+            Bulb obj = new Bulb();
+            switch (input) {
+                case 1:
+                    obj.bulbOn();
+                    break;
+                case 2:
+                    obj.bulbOff();
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("enter valid input");
+            }
 
+        } while (input != 0);
     }
 }
 
